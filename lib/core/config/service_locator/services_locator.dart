d5bee582c20462429ebbfc4143d426650533cdb5
Israel Michael Akpan <isrealakpan296@gmail.com>
2021-12-25 22:34:46 +0100

    registered wallet service

--- lib/core/config/service_locator/services_locator.dart
+++ lib/core/config/service_locator/services_locator.dart
@@ -4,19 +4,27 @@ import 'package:wirepay/core/config/services/network_service/network_service_imp
 import 'package:wirepay/core/config/services/storage_service/storage_service.dart';
 import 'package:wirepay/modules/login/data/repositories/login_repo.dart';
 import 'package:wirepay/modules/login/data/services/login_service.dart';
+import 'package:wirepay/modules/wallet_module/data/repositories/wallet_repo.dart';
+import 'package:wirepay/modules/wallet_module/data/services/wallet_service.dart';
 
 GetIt di = GetIt.instance;
 
 Future<void> registerAllDependencies() async {
   // Register all services
-  di.registerLazySingleton<NetworkService>(() => NetworkServiceImpl());
   di.registerLazySingleton<StorageService>(() => StorageService());
+  di.registerLazySingleton<NetworkService>(() => NetworkServiceImpl());
   di.registerLazySingleton<LoginService>(() => LoginService());
+  di.registerLazySingleton<WalletService>(() => WalletService());
 
   // Register all repositories
   di.registerLazySingleton<LoginRepository>(
     () => LoginRepository(
       loginService: di(),
+    ),
+  );
+  di.registerLazySingleton<WalletRepository>(
+    () => WalletRepository(
+      walletService: di(),
     ),
   );
 }
